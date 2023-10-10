d015274f62145bd065d2be6c52bbb9d21d820868
fabrobrd <74380803+fabrobrd@users.noreply.github.com>
2023-10-10 16:21:47 -0300

    saveclaims

--- IdentityUsers/IdentityUsers/Areas/Identity/Pages/Account/Claims.cshtml.cs
+++ IdentityUsers/IdentityUsers/Areas/Identity/Pages/Account/Claims.cshtml.cs
@@ -21,6 +21,9 @@ using System.Security.Claims;
 using IdentityUsers.Migrations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Http.Features;
+using Abakon.SharedKernel.Domain;
+using MessagePack.Formatters;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace IdentityUsers.Areas.Identity.Pages.Account
 {
@@ -61,17 +64,9 @@ namespace IdentityUsers.Areas.Identity.Pages.Account
 
         public async Task OnGetAsync(string returnUrl = null)
         {
+            ClaimsPropierties = await ListOfClaims();
             ReturnUrl = returnUrl;
-            IList<ClaimEntity> claims = await GetAllClaimsActive();
-            ClaimsPropierties = new List<ViewModel>();
-            foreach (var items in claims.OrderBy(x => x.Name))
-            {
-                var list = new ViewModel();
-                list.Id = items.Id;
-                list.ClaimName = items.Name;
-                list.ClaimValue = items.Value;
-                ClaimsPropierties.Add(list);
-            }
+
         }
 
         protected async Task<IList<ClaimEntity>> GetAllClaimsActive()
@@ -82,19 +77,41 @@ namespace IdentityUsers.Areas.Identity.Pages.Account
                             select cl).ToListAsync();
             return claims;
         }
-//        public async Task<IActionResult> OnPostAsync(InputModel model, string returnUrl = null)
-        public async Task<IActionResult> OnPostAsync(InputModel model)
+
+        protected async Task<List<ViewModel>> ListOfClaims()
+        {
+            IList<ClaimEntity> claims = await GetAllClaimsActive();
+            List<ViewModel> claimsPropierties = new List<ViewModel>();
+            if (claims.Count != 0)
+            {
+                foreach (var items in claims.OrderBy(x => x.Name))
+                {
+                    var list = new ViewModel();
+                    list.Id = items.Id;
+                    list.ClaimName = items.Name;
+                    list.ClaimValue = items.Value;
+                    claimsPropierties.Add(list);
+                }
+            }
+            return claimsPropierties;
+
+
+        }
+
+        //        public async Task<IActionResult> OnPostAsync(InputModel model, string returnUrl = null)
+        public async Task<IActionResult> OnPostAsync(InputModel Input)
         {
             string returnUrl = Url.Content("~/");
             if (ModelState.IsValid)
             {
                 var user = CreateClaim();
-                if (await FindByClaimNameAsync(model.ClaimName) == false)
+                if (await FindByClaimNameAsync(Input.ClaimName) == false)
                 {
-                    await SaveClaimEntity(model);
+                    await SaveClaimEntity(Input);
                 }
 
             }
+            ClaimsPropierties = await ListOfClaims();
             return Page();
         }
         private ApplicationUser CreateClaim()
@@ -120,12 +137,65 @@ namespace IdentityUsers.Areas.Identity.Pages.Account
 
         protected async Task SaveClaimEntity(InputModel model)
         {
+            
             ClaimEntity claimsEntity = new ClaimEntity();
             claimsEntity.Name = model.ClaimName;
             claimsEntity.Value = model.ClaimValue;
             claimsEntity.Active = true;
-            await _context.SaveChangesAsync();
+            await _context.ClaimsEntity.AddAsync(claimsEntity);
+            try
+            {
+                var result = await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException(ex.Message);
+            }
         }
+        public async Task<IActionResult> OnDeleteAsync(int id)
+        {
+            string returnUrl = Url.Content("~/");
+
+            ClaimEntity claimRemoved = await _context.ClaimsEntity.FindAsync(id);
+            bool searchClaim = await checkIfTheClaimIsUsed(claimRemoved.Name);
+            if (searchClaim)
+            {
+                var deleted = _context.ClaimsEntity.Remove(claimRemoved);
+                int saved = await _context.SaveChangesAsync();
+            }
+
+            ClaimsPropierties = await ListOfClaims();
+            return Page();
+        }
+
+        protected async Task<bool> checkIfTheClaimIsUsed(string claimName)
+        {
+
+            Claim claim = new Claim(claimName, "");
+            var result = await _userManager.GetUsersForClaimAsync(claim);
+            if (result.Any())
+            {
+                throw new InvalidOperationException("No se puede eliminar el CLAIM, hay usuarios utilizándolo");
+            }
+            return true;
+        }
+
+        public async Task<IActionResult> OnPutAsync(int id)
+        {
+            string returnUrl = Url.Content("~/");
+            ClaimEntity claimRemoved = await _context.ClaimsEntity.FindAsync(id);
+            bool searchClaim = await checkIfTheClaimIsUsed(claimRemoved.Name);
+
+            
+
+            if (searchClaim)
+            {
+            }
+
+                ClaimsPropierties = await ListOfClaims();
+            return Page();
+        }
+
     }
 }
 
